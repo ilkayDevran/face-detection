99f3d3b9c7d38f66ce37960ce4ff0fdbcbdcaf8b
ilkay <ilkaytevfikdevran@gmail.com>
2019-06-14 08:56:21 +0300

    haar with different parameters

--- face_chopper.py
+++ face_chopper.py
@@ -3,49 +3,55 @@ import cv2
 import numpy as np
 import os
 
-#import classifier for face and eye detection
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# Import Classifier for Face and Eye Detection
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-r = cv2.CascadeClassifier ('haarcascade_profileface.xml')
-def face_detector (img, size=0.5):
-	try:
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	except Exception as e:
-		return 0,img
 
-	
-	faces = face_classifier.detectMultiScale(gray,1.3,5)
-	if faces is ():
-		faces = r.detectMultiScale(gray,1.3,5)
-		if faces is ():
+class faceChopper:
+	def __init__(self):
+		# Import Classifier for Face and Eye Detection
+		self.frontal_face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+		self.profile_face_detector = cv2.CascadeClassifier('haarcascade_profileface.xml')
+
+	def face_detector(self, img, size=0.5):
+		try:
+			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		except Exception as e:
 			return 0,img
-	# Given coordinates to detect face and eyes location from ROI
-	print len (faces)
-	roi_color=[]
 
-	for (x, y, w, h) in faces:
-		c=15
-		x = x - c 
-		w = w + c 
-		y = y - c 
-		h = h + c
-		roi_gray = gray[y: y+h, x: x+w]
-		roi_color.append( img[y: y+h, x: x+w])
-	leng= roi_color.__len__()
-	print leng
-	return leng,roi_color
-		# Webcam setup for Face Detection
+		faces = self.frontal_face_detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+		if faces is ():
+			faces = self.profile_face_detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+			if faces is ():
+				return 0, img
+		
+		# Given coordinates to detect face and eyes location from ROI
+		print(len(faces))
+		roi_color=[]
+		for (x, y, w, h) in faces:
+			c = 15
+			x = x - c 
+			w = w + c 
+			y = y - c 
+			h = h + c
+			roi_gray = gray[y: y+h, x: x+w]
+			roi_color.append( img[y: y+h, x: x+w])
+
+		print(roi_color.__len__())
+		return roi_color.__len__(), roi_color
 
 
+def main():
+	fC = faceChopper()
 
-n=0
-for path in os.listdir("inputs"):
-	print path
-	cap = cv2.imread("inputs/"+path)
-	#cap = cv2.resize(cap, (400, 300)) 
-	
-	leng ,frames = face_detector (cap)
-	for i in range (0,leng):
-		cv2.imwrite("outputs/test%d.jpg" % n ,frames[i])
-		n+=1
+	for imageName in os.listdir("inputs"):
+		if imageName != '.DS_Store':
+			face_counter=0
+			print(imageName)
+			cap = cv2.imread("inputs/"+imageName)
+			#cap = cv2.resize(cap, (400, 300)) 
+			_,frames = fC.face_detector (cap)
+			for frame in frames:
+				cv2.imwrite(os.path.join("outputs", imageName.replace('.jpg','') + "-face%d.jpg" % face_counter), frame)
+				face_counter+=1
+
+
+if __name__ == "__main__":
+	main()
---
+++ face_detector.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import glob
+
+
+
+txtfiles = [] 
+for file in glob.glob("*.jpg"):
+    txtfiles.append(file)
+    
+for ix in txtfiles:
+    img = cv2.imread(ix,cv2.IMREAD_COLOR)
+    imgtest1 = img.copy()
+    imgtest = cv2.cvtColor(imgtest1, cv2.COLOR_BGR2GRAY)
+    facecascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    profile_cascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+   
+    faces = facecascade.detectMultiScale(imgtest, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
+    print('Total number of Faces found',len(faces))
+    for (x, y, w, h) in faces:
+        face_detect = cv2.rectangle(imgtest, (x, y), (x+w, y+h), (255, 255, 255), 2)
+        roi_gray = imgtest[y:y+h, x:x+w]
+        roi_color = imgtest[y:y+h, x:x+w]        
+        plt.imshow(face_detect)
+    #plt.show()
+
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    print('Total number of Eyes found',len(eyes))
+    for (ex,ey,ew,eh) in eyes:
+        eye_detect = cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,0),2)
+        plt.imshow(eye_detect)
+    #plt.show()
+
+    profile_faces = profile_cascade.detectMultiScale(imgtest, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
+    print('Total number of Profile Faces found',len(profile_faces))
+    for (x, y, w, h) in profile_faces:
+        profile_face_detect = cv2.rectangle(imgtest, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        roi_gray = imgtest[y:y+h, x:x+w]
+        roi_color = imgtest[y:y+h, x:x+w]        
+        plt.imshow(profile_face_detect)
+    plt.show()
+
+print("Done")
