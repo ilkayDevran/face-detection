5445bcf47c772dcc018fcaca0e4d997fb9760ca9
ilkay <ilkaytevfikdevran@gmail.com>
2019-06-14 09:23:27 +0300

    eye detector removed

--- face_detector.py
+++ face_detector.py
@@ -20,23 +20,25 @@ for ix in txtfiles:
     faces = facecascade.detectMultiScale(imgtest, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
     print('Total number of Faces found',len(faces))
     for (x, y, w, h) in faces:
-        face_detect = cv2.rectangle(imgtest, (x, y), (x+w, y+h), (255, 255, 255), 2)
+        face_detect = cv2.rectangle(imgtest, (x, y), (x+w, y+h), (0, 0, 0), 2)
         roi_gray = imgtest[y:y+h, x:x+w]
         roi_color = imgtest[y:y+h, x:x+w]        
         plt.imshow(face_detect)
     #plt.show()
 
+    """
     eyes = eye_cascade.detectMultiScale(roi_gray)
     print('Total number of Eyes found',len(eyes))
     for (ex,ey,ew,eh) in eyes:
         eye_detect = cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,0),2)
         plt.imshow(eye_detect)
     #plt.show()
+    """
 
     profile_faces = profile_cascade.detectMultiScale(imgtest, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
     print('Total number of Profile Faces found',len(profile_faces))
     for (x, y, w, h) in profile_faces:
-        profile_face_detect = cv2.rectangle(imgtest, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        profile_face_detect = cv2.rectangle(imgtest, (x, y), (x+w, y+h), (255, 0, 255), 2)
         roi_gray = imgtest[y:y+h, x:x+w]
         roi_color = imgtest[y:y+h, x:x+w]        
         plt.imshow(profile_face_detect)
